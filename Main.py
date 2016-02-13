700ba1d6c40f00f20156d10abe16bb21ca371c0f
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-02-13 23:07:33 +0000

    Entry point added.

---
+++ Main.py
@@ -0,0 +1,19 @@
+import energy_reviews
+
+def main():
+   client = energy_reviews.TwitterClient
+   #query = energy_review.TwitterQuery
+   #query = energy_review.TwitterQuery.set_q("edfenergy")
+   #tweets = client.get_tweets(query)
+   #for tweet in tweets:
+   #    print "Tweet: " + tweet
+
+   #energy_reviews.getTweets("eon")
+   #energy_reviews.getTweets("npower")
+   #energy_reviews.getTweets("BritishGas")
+
+
+if __name__ == '__main__':
+        main()
+
+
