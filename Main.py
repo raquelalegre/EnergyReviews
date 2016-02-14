4cc243826eb3e01466df2e26831361854695bd0c
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-02-14 10:19:37 +0000

    Test new query functionality

--- Main.py
+++ Main.py
@@ -3,17 +3,18 @@ import energy_reviews
 def main():
    client = energy_reviews.TwitterClient
    query = energy_reviews.TwitterQuery
-   #query = energy_review.TwitterQuery.set_q("edfenergy")
-   #tweets = client.get_tweets(query)
-   #for tweet in tweets:
-   #    print "Tweet: " + tweet
+   query.set_q("edf", "edfenergy", "edfenergycs")
+   query.set_count(2000)
+   query.set_since('2015-01-01')
+   query.set_until('2016-02-20')
+   tweets = client.get_tweets(query)
+
+   for tweet in tweets:
+       print "Tweet: " + tweet
 
    #energy_reviews.getTweets("eon")
    #energy_reviews.getTweets("npower")
    #energy_reviews.getTweets("BritishGas")
 
-
 if __name__ == '__main__':
         main()
-
-
